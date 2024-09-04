6bc6e290763f7854b0ba9616981dec3f0e4d24a5
tsukimi <128211112+hikawi@users.noreply.github.com>
2024-09-04 20:24:32 +0700

    fix some color mismatch

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -6,10 +6,10 @@
 </script>
 
 <div
-  class="{className} border-t-4 row-span-2 flex flex-col justify-between w-card h-card p-6 lg:p-8 rounded-lg shadow-lg duration-100 hover:scale-105"
+  class="{className} border-t-4 row-span-2 flex flex-col justify-between w-card h-card p-6 lg:p-8 rounded-lg shadow-lg duration-100 hover:scale-105 bg-white"
 >
   <div class="text-left flex flex-col gap-2">
-    <h1 class="font-semibold text-2xl">
+    <h1 class="font-semibold text-xl">
       {title}
     </h1>
     <p class="text-sm text-neutral-very-dark-blue opacity-50">
